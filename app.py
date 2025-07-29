243cf67e3ab7b4a55ceb659b17662cc9098dd847
Sardor20082 <xfiles20242008@gmail.com>
2025-07-29 19:12:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from main import app
+from flask import Flask, request
+import config
+import handlers
 
-if __name__ == "__main__":
-    app.run()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Bot ishlayapti!'
+
+@app.route(f"/{config.BOT_TOKEN}", methods=['POST'])
+def webhook():
+    if request.method == "POST":
+        update = request.get_json()
+        handlers.handle_update(update)
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
